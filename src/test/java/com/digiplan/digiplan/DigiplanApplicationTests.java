c77cf75beebb79ba8f9e287718f637165ebc7509
waid <cheikhdiaw0411@gmail.com>
2022-05-25 12:44:55 +0000

    commit jar

--- src/test/java/com/digiplan/digiplan/DigiplanApplicationTests.java
+++ src/test/java/com/digiplan/digiplan/DigiplanApplicationTests.java
@@ -3,7 +3,7 @@ package com.digiplan.digiplan;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = DigiplanApplicationTests.class)
 class DigiplanApplicationTests {
 
 	@Test
